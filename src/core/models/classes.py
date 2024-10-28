aaa926dbb044260b0993f835e866892b3f6f79e2
Amir <xxxtofi666@gmail.com>
2024-10-28 16:04:21 +0500

    ...

--- src/core/models/classes.py
+++ src/core/models/classes.py
@@ -2,8 +2,8 @@ from sqlalchemy import Table, Column, String, Integer, Boolean, JSON
 from src.db.database import metadata
 
 
-gods = Table(
-    'gods',
+classes = Table(
+    'classes',
     metadata,
     Column('id', Integer, primary_key=True),
     Column('name', String),
@@ -12,9 +12,9 @@ gods = Table(
     Column('picture_url', String),
 
     # Все значения взяты с таблицы users.
-    Column('needs', JSON, default='{}'),  # Что нужно от игрока для поклонения
-    Column('changes', JSON, default='{}'),   # Баффы и Нерфы для игрока
-    Column('fight_changes', JSON, default='{}')   # Баффы и Нерфы для игрока во время боя
+    Column('needs', JSON, default='{}'),  # Что нужно от игрока для класса
+    Column('changes', JSON, default='{}'),  # Баффы и Нерфы для игрока
+    Column('fight_changes', JSON, default='{}')  # Баффы и Нерфы для игрока во время боя
     # пример условий во время боя:
     # {поле врага: {значение поля:{поле игрока: значение которое нужно прибавить}}}
     # {}
--- src/core/models/users.py
+++ src/core/models/users.py
@@ -12,27 +12,27 @@ users = Table(
     Column('age', Integer),
     Column('max_age', Integer),
     Column('gold', Integer),
-    Column('min_atk', Integer),
-    Column('max_atk', Integer),
+    Column('karma', Integer),
+    Column('strength', Integer),
+    Column('mana', Integer),
     Column('exp_points', Integer),
     Column('title', Integer),
+    Column('type', String, default='player'),
 
-    Column('class_id', Integer),  # Должен быть привязан к таблице классов
+    Column('can_speak', Boolean, default=True),
+    Column('can_move', Boolean, default=True),
+    Column('can_kill_players', Boolean, default=True),
+
+    Column('class_id', Integer),
     Column('potential_id', Integer),
-    Column('god_id', Integer),
-    Column('weakness_id', Integer),
+    Column('god_id', Integer, default=1),  # Нужен дефолтный
+    Column('weakness_id', Integer, default=None),
     Column('born_kingdom_id', Integer),
     Column('current_location_id', Integer),
     Column('citizen_kingdom_id', Integer),
 
-    Column('magic_lvl', Integer, default=5),
-    Column('physical_lvl', Integer, default=5),
-    Column('agility_lvl', Integer, default=5),
-    Column('carma_lvl', Integer, default=5),
-
     Column('avatar_prompt', String, default=settings.DEFAULT_STYLE_PROMPT),
     Column('avatar_url', String),
-    Column('avatar_can_change', Boolean, default=False),
 
     Column('dungeon_cleared', Integer),
     Column('monsters_killed', Integer),
